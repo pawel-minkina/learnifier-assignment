15adc8e928a76a951abbd82d1b49f312e8fcb309
minkina <pawel.minkina@luxdone.com>
2021-05-04 17:56:13 +0200

    [deploy] 04.05.2020

---
+++ precache-manifest.7e0236ce77f67cd32d071f7999bc3103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d277c9ec9847459fc870d41ca40e7fb",
+    "url": "/learnifier-assignment/index.html"
+  },
+  {
+    "revision": "7609c92632103309d520",
+    "url": "/learnifier-assignment/static/css/2.7ca58fb4.chunk.css"
+  },
+  {
+    "revision": "9377352a9b965acdce32",
+    "url": "/learnifier-assignment/static/css/main.9cdd9fb7.chunk.css"
+  },
+  {
+    "revision": "7609c92632103309d520",
+    "url": "/learnifier-assignment/static/js/2.4be557a7.chunk.js"
+  },
+  {
+    "revision": "9377352a9b965acdce32",
+    "url": "/learnifier-assignment/static/js/main.2d295454.chunk.js"
+  },
+  {
+    "revision": "f72cca6223a27bb9fa4b",
+    "url": "/learnifier-assignment/static/js/runtime~main.7a543556.js"
+  }
+]);
