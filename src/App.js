0852830f8c0a67412e0a8588c13f0418616a518f
minkina <pawel.minkina@luxdone.com>
2021-05-04 10:32:42 +0200

    initial app configuration / fork

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container} from 'reactstrap';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Container>
+                STUB
+            </Container>
+        </div>
+    );
+}
+
+export default App;
